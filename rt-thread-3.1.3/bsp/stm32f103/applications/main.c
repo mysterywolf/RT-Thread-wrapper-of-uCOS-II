96347fee714644993f2da0467917bc474ce8703e
mysterywolf <920369182@qq.com>
2020-09-17 02:12:07 +0800

    add files

--- rt-thread-3.1.3/bsp/stm32f103/applications/main.c
+++ rt-thread-3.1.3/bsp/stm32f103/applications/main.c
@@ -5,14 +5,14 @@
  *
  * Change Logs:
  * Date           Author       Notes
- * 2020-09-17     Meco Man     implement uCOS-II Wrapper
+ * 2020-09-17     Meco Man     first version
  */
 
 /*本文件展示了兼容层实现的uCOS-II官方给出的标准初始化流程*/
 /*启动以及初始化过程严格遵照官方给出的例程*/
 /*串口使用USART2*/
 
-//#include <os.h>
+#include <os.h>
 
 int main(void)/*RT-Thread main线程*/
 {
--- uCOS-II_Wrapper/uCOS-II/ucos_ii.h
+++ uCOS-II_Wrapper/uCOS-II/ucos_ii.h
@@ -54,7 +54,7 @@ extern "C" {
 #include <app_cfg.h>
 #include <os_cfg.h>
 #include <os_cpu.h>
-#include "os_trace.h"
+//#include "os_trace.h"
 
 /*
 *********************************************************************************************************
